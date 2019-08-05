29ae0cb56ddf81d48a689882feb589d0d83025b6
Drew Beaupre <drew@beauprefamily.org>
2019-08-05 15:35:50 -0700

    campaign site day report is grouped by 'site', so adding aggregation to make rows unique

--- models/fivetran/base/taboola_campaign_performance.sql
+++ models/fivetran/base/taboola_campaign_performance.sql
@@ -3,11 +3,11 @@ with performance_table as(
 
         date_time::DATE as date_day,
         campaign_id,
+        site,
         impressions,
         clicks,
         cpa_actions_num as conversions,
-        spent as spend,
-        currency
+        spent as spend
 
     from {{source('taboola_ads', 'CAMPAIGN_SITE_DAY_REPORT')}}
  
@@ -17,10 +17,11 @@ select
     {{ dbt_utils.surrogate_key('date_day', 'campaign_id') }}::varchar as id,
     date_day,
     campaign_id,
-    impressions,
-    clicks,
-    conversions,
-    spend,
-    currency
+    sum(impressions) impressions,
+    sum(clicks) clicks,
+    sum(conversions) conversions,
+    sum(spend) spend,
 
 from performance_table
+{{ dbt_utils.group_by(3) }}
+
